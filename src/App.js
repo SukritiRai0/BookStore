6c410f1067a980b7d1db379a892f0e8cd3a224bc
Daniel Lu <daniellu@pal-nat186-147-112.itap.purdue.edu>
2019-09-14 15:47:43 -0400

    added a text box and button

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Purdue BookStore
         </p>
+        <input type="text" id="idTxt"></input>
+        <input type="button" id="idSearchButton" value="Search"></input>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
